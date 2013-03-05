32b9976296a5e6b26851cf8115373d734beffaef
Roman Roibu <roman.roibu@gmail.com>
2013-03-06 00:03:06 +0200

    Add extended functionality: chosing title page, body pages, output file and show help

--- RRG.rb
+++ RRG.rb
@@ -1,50 +1,92 @@
-#!/usr/bin/env ruby
+#!/usr/bin/env ruby -W0
 
-# Check if "Title.tex" exists
-unless File.exists? "Title.tex"
-  puts "Error: \"Title.tex\" not found"
+# Set initial configurations
+title  = "Title.tex"
+body   = ["README.md"]
+report = "Report.pdf"
+
+def show_help
+  puts  "Help:", "\n",
+        "-h | -help   | --help\t\t\tShow help",
+        "-t | -title  | --title  [file]\t\tSpecify title page (.tex)",
+        "-o | -output | --output [file]\t\tSpecify output filename (.pdf)", "\n",
+        "All other arguments will be treated as report pages"
+end
+
+# Simple option parser
+unless ARGV.empty?
+  arg = ARGV.shift
+  case arg
+  when '-h', '-help', '--help'
+    show_help
+    exit
+  when '-t', '-title', '--title'
+    title = ARGV.shift
+  when '-o', '-output', '--output'
+    report = ARGV.shift
+  else
+    body = ARGV
+  end
+end 
+
+# Check if title file exists
+unless File.exists? title
+  puts "Error: \"#{title}\" not found"
   exit
 end
 
-# Check if "README.md" exists
-unless File.exists? "README.md"
-  puts "Error: \"README.md\" not found"
-  exit
+# Check if body files exist
+body.each do |doc|
+  unless File.exists? doc
+    puts "Error: \"#{doc}\" not found"
+    exit
+  end
 end
 
-# Change URL of images to a local path, in README.md
-# Get the contents of "README.md"
-readme = File.open("README.md", "rb").read
-# Get an array of image URLs
-urls = readme.scan(/!\[\w*\]\((\S*)\)/).flatten
-# Get local paths to those images
-# It assumes your pictures are all in a folder in lab dir
-paths = urls.map{|url| url.split("/").last(2).join("/")}
-# Substitute URLs with local paths
-urls.each_with_index do |url, i| readme.gsub! url, paths[i] end
-# Write a modified README.md => "README_local.md"
-File.open("README_local.md", 'w') { |file| file.write readme }
+# Array of modified files
+files = []
 
-# Set postfix (ex: "lab#1", if current dir is "lab#1")
-postfix = Dir.pwd.split(/[\/|\\]/).last
+### Change URLs of images to local path in body files
+body.each_with_index do |doc, idx|
+  # Get the contents of file
+  file = File.open(doc, 'rb').read
+
+  # Get an array of image URLs
+  urls = file.scan(/!\[\w*\]\((\S*)\)/).flatten
+
+  # Get local paths to images, assuming they are all in a folder in repo
+  paths = urls.map{|url| url.split("/").last(2).join("/")}
+
+  # Substitute URLs with local paths
+  urls.each_with_index do |url, i| file.gsub! url, paths[i] end
+
+  # Write the modified file: file#{idx}.md
+  File.open("file\##{idx}.md", 'w') { |f| f.write file }
+
+  # Add file name to array of modified files, to work with later
+  files << "file\##{idx}.md"
+end
 
 # Create "temp" directory
 system "mkdir temp"
 
-# Create "Content.pdf" in "temp" dir
-system "pandoc README_local.md -o temp/Content.pdf"
+# Report body PDFs
+pdfs = []
+
+# Create report body PDFs
+files.each do |file|
+  pdfs << file.sub(/(.md)/, ".pdf")
+  system "pandoc #{file} -o temp/#{pdfs.last}"
+end
 
 # Change current dir to "temp"
 Dir.chdir "temp"
 
-# Create "Title.pdf" and other LaTeX junk in "temp" dir
-system "pdflatex ../Title.tex"
+# Create title PDF, as well as other LaTeX junk
+system "pdflatex ../#{title}"
 
-# Set report filename (ex: "Report_lab#1.pdf")
-filename = "Report_" + postfix + ".pdf"
-
-# Merge "Title.pdf" and "Content.pdf" into "../Report"
-system "gs -sDEVICE=pdfwrite -dNOPAUSE -dBATCH -dSAFER -sOutputFile=../#{filename} Title.pdf Content.pdf"
+# Merge title PDF and body PDFs int report
+system "gs -sDEVICE=pdfwrite -dNOPAUSE -dBATCH -dSAFER -sOutputFile=../#{report} #{title.sub /(.tex)/, ".pdf"} #{pdfs.join(' ')}"
 
 # Up dir
 Dir.chdir ".."
@@ -52,12 +94,8 @@ Dir.chdir ".."
 # Remove "temp" dir
 system "rm -Rf temp"
 
-# Remove "README_local.md"
-system "rm README_local.md"
-
-
-
-
-
-
+# Remove .md body files
+files.each do |file|
+  system "rm #{file}"
+end
 
